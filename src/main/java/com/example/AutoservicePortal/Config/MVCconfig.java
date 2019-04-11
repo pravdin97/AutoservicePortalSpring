406b5a19426be2e64e57315934900120dab326a8
pravdin97 <pravdin.ev97@gmail.com>
2019-04-11 15:09:43 +0700

    добавил конфиги, страницу логина не работают стили

--- src/main/java/com/example/AutoservicePortal/Config/MVCconfig.java
+++ src/main/java/com/example/AutoservicePortal/Config/MVCconfig.java
@@ -2,6 +2,7 @@ package com.example.AutoservicePortal.Config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -10,5 +11,10 @@ public class MVCconfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
     }
 }
